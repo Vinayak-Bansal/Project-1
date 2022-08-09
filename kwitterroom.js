789ce67192e92561a237baa08e996cc35dad2c5d
Vinayak-Bansal <97925994+Vinayak-Bansal@users.noreply.github.com>
2022-08-08 19:53:23 -0500

    Add files via upload

---
+++ kwitterroom.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCuEcR5GkJJxcgNJ_q_LvIICeB2dspBOPc",
+    authDomain: "lets-chatt-web-app.firebaseapp.com",
+    databaseURL: "https://lets-chatt-web-app-default-rtdb.firebaseio.com",
+    projectId: "lets-chatt-web-app",
+    storageBucket: "lets-chatt-web-app.appspot.com",
+    messagingSenderId: "236012059073",
+    appId: "1:236012059073:web:ea4c5fa9901ea539e05740"
+  };
